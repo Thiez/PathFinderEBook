e14d6ecdaba6bd066860705788c4b09d1ea23fc7
Matthijs Hofstra <thiezz@gmail.com>
2017-06-01 09:35:00 +0200

    Almost working version

--- Program.cs
+++ Program.cs
@@ -12,8 +12,13 @@ namespace SpellEbook
 {
     public class Program
     {
-        private const string XhtmlHeader = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n";
+        private const string XhtmlHeader = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">\n";
         private const string InputFile = "spell_full - Updated 20Nov2016.csv";
+
+        private static readonly ISet<char> BadChars = new HashSet<char>(Path.GetInvalidPathChars())
+        {
+            ' ', ',', '\''
+        };
 
         public static void Main(string[] args)
         {
@@ -24,9 +29,9 @@ namespace SpellEbook
             var spells = csvEntries
                 .Skip(1)
                 .Select(Spell.FromCsvEntry)
-                .Where(s => s.GetSpellLevels().Any(kv => interestingClasses.Contains(kv.Key) && kv.Value == 3))
+                //.Where(s => s.GetSpellLevels().Any(kv => interestingClasses.Contains(kv.Key)))
                 .OrderBy(s => s.Name)
-                .Take(60)
+                //.Take(10)
                 .ToList();
 
             var dir = new DirectoryInfo("book");
@@ -38,31 +43,144 @@ namespace SpellEbook
             {
                 // ignored
             }
-            
-            using (var archive = new ZipArchive(File.Open(Path.Combine(dir.FullName, "book.zip"), FileMode.Create), ZipArchiveMode.Create))
+
+            using (var archive = new ZipArchive(File.Open(Path.Combine(dir.FullName, "book.epub"), FileMode.Create), ZipArchiveMode.Create))
             {
                 GenerateBoilerplate(archive, spells);
+                GenerateSpells(archive, spells);
             }
-            
-            
-            Directory.CreateDirectory(Path.Combine(dir.FullName, "OEBPS"));
-            foreach (var spell in spells)
+        }
+
+        private static void GenerateSpells(ZipArchive archive, List<Spell> spells)
+        {
+            const string bookId = "PathfinderSpellEBook";
+
+            const string opfNS = "http://www.idpf.org/2007/opf";
+            const string dcNS = "http://purl.org/dc/elements/1.1/";
+
+            XmlElement manifest;
+            XmlElement spine;
             {
-                var xml = $"{XhtmlHeader}{ToXhtml(spell, interestingClasses).OuterXml}";
-                var fileNameChars = (spell.Name + ".xhtml").ToCharArray();
-                var badChars = Path.GetInvalidFileNameChars();
-                for (var i = 0; i < fileNameChars.Length; i++)
+                var contentDoc = new XmlDocument();
+                contentDoc.AppendChild(contentDoc.CreateXmlDeclaration("1.0", "UTF-8", null));
+                contentDoc.AppendChild(contentDoc.CreateElement("package", opfNS));
+                var opfRoot = contentDoc.DocumentElement;
+                opfRoot.SetAttribute("unique-identifier", bookId);
+                opfRoot.SetAttribute("version", "2.0");
+                var metaData = contentDoc.CreateElement("metadata", opfNS);
+                opfRoot.AppendChild(metaData);
+                metaData.SetAttribute("xmlns:dc", dcNS);
+                metaData.SetAttribute("xmlns:opf", opfNS);
+
+                var title = contentDoc.CreateElement("dc", "title", dcNS);
+                metaData.AppendChild(title);
+                title.AppendChild(contentDoc.CreateTextNode(bookId));
+                
+                var creator = contentDoc.CreateElement("dc", "creator", dcNS);
+                metaData.AppendChild(creator);
+                creator.SetAttribute("role", opfNS, "aut");
+                creator.AppendChild(contentDoc.CreateTextNode("Matthijs Hofstra"));
+
+                var language = contentDoc.CreateElement("dc", "language", dcNS);
+                metaData.AppendChild(language);
+                language.AppendChild(contentDoc.CreateTextNode("en-US"));
+
+                var rights = contentDoc.CreateElement("dc", "rights", dcNS);
+                metaData.AppendChild(rights);
+                rights.AppendChild(contentDoc.CreateTextNode("Public Domain"));
+
+                var publisher = contentDoc.CreateElement("dc", "publisher", dcNS);
+                metaData.AppendChild(publisher);
+                publisher.AppendChild(contentDoc.CreateTextNode("Nobody"));
+
+                var identifier = contentDoc.CreateElement("dc", "identifier", dcNS);
+                metaData.AppendChild(identifier);
+                identifier.SetAttribute("id", bookId);
+                identifier.SetAttribute("scheme", opfNS, "UUID");
+                identifier.AppendChild(contentDoc.CreateTextNode(bookId));
+
+                manifest = contentDoc.CreateElement("manifest", opfNS);
+                opfRoot.AppendChild(manifest);
+
+                var ncx = contentDoc.CreateElement("item", opfNS);
+                manifest.AppendChild(ncx);
+                ncx.SetAttribute("id", "toc.ncx");
+                ncx.SetAttribute("href", "toc.ncx");
+                ncx.SetAttribute("media-type", "application/x-dtbncx+xml");
+
+                var css = contentDoc.CreateElement("item", opfNS);
+                manifest.AppendChild(css);
+                css.SetAttribute("id", "style");
+                css.SetAttribute("href", "Styles/Style.css");
+                css.SetAttribute("media-type", "text/css");
+
+                spine = contentDoc.CreateElement("spine", opfNS);
+                opfRoot.AppendChild(spine);
+                //var itemRef = contentDoc.CreateElement("itemref", opfNS);
+                //spine.AppendChild(itemRef);
+                //itemRef.SetAttribute("idref", "toc");
+                spine.SetAttribute("toc", "toc.ncx");
+            }
+
+            const string ncxNS = "http://www.daisy.org/z3986/2005/ncx/";
+            XmlElement navMap;
+            {
+                
+                var tocDoc = new XmlDocument();
+                tocDoc.AppendChild(tocDoc.CreateXmlDeclaration("1.0", "UTF-8", null));
+                tocDoc.AppendChild(tocDoc.CreateElement("ncx", ncxNS));
+                tocDoc.DocumentElement.SetAttribute("version", "2005-1");
+
+                var head = tocDoc.CreateElement("head", ncxNS);
+                tocDoc.DocumentElement.AppendChild(head);
+                
+                var metaData = new Dictionary<string, string>
                 {
-                    if (badChars.Contains(fileNameChars[i]))
-                    {
-                        fileNameChars[i] = '_';
-                    }
+                    { "dtd:uid", bookId },
+                    { "dtd:depth", "1"},
+                    { "dtd:totalPageCount", "0" },
+                    { "dtd:maxPageNumber", "0" }
+                };
+
+                foreach (var kv in metaData)
+                {
+                    var meta = tocDoc.CreateElement("meta", ncxNS);
+                    head.AppendChild(meta);
+                    meta.SetAttribute("name", kv.Key);
+                    meta.SetAttribute("content", kv.Value);
                 }
 
-                File.WriteAllText(Path.Combine(dir.FullName, "OEBPS", new string(fileNameChars)), xml);
+                var title = tocDoc.CreateElement("docTitle", ncxNS);
+                tocDoc.DocumentElement.AppendChild(title);
+                var text = tocDoc.CreateElement("text", ncxNS);
+                title.AppendChild(text);
+                text.AppendChild(tocDoc.CreateTextNode(bookId));
+
+                navMap = tocDoc.CreateElement("navMap", ncxNS);
+                tocDoc.DocumentElement.AppendChild(navMap);
             }
 
-            var classToSpells = interestingClasses.ToDictionary(c => c, c => new List<Spell>(), StringComparer.OrdinalIgnoreCase);
+            var classes = spells.SelectMany(s => s.GetSpellLevels().Select(kv => kv.Key)).Distinct().ToList();
+            foreach (var spell in spells)
+            {
+                var entry = archive.CreateEntry(CreateEntryName($"OEBPS/{spell.Name}.xhtml"));
+                using (var writer = new StreamWriter(entry.Open(), Encoding.UTF8))
+                {
+                    writer.Write(XhtmlHeader);
+                    writer.Write(ToXhtml(spell, classes).OuterXml);
+                }
+
+                var item = manifest.OwnerDocument.CreateElement("item", opfNS);
+                manifest.AppendChild(item);
+                item.SetAttribute("id", entry.Name);
+                item.SetAttribute("href", entry.Name);
+                item.SetAttribute("media-type", "application/xhtml+xml");
+                var itemRef = spine.OwnerDocument.CreateElement("itemref", opfNS);
+                spine.AppendChild(itemRef);
+                itemRef.SetAttribute("idref", entry.Name);
+            }
+
+            var classToSpells = classes.ToDictionary(c => c, c => new List<Spell>(), StringComparer.OrdinalIgnoreCase);
             foreach (var spell in spells)
             {
                 foreach (var kv in spell.GetSpellLevels())
@@ -75,7 +193,9 @@ namespace SpellEbook
                 }
             }
 
-            foreach (var kv in classToSpells)
+            var manifestFragment = manifest.OwnerDocument.CreateDocumentFragment();
+            var spineFragment = spine.OwnerDocument.CreateDocumentFragment();
+            foreach (var kv in classToSpells.OrderBy(kv => kv.Key))
             {
                 var doc = new AngleSharp.Parser.Html.HtmlParser().Parse(string.Empty);
                 var html = (AngleSharp.Dom.IElement)doc.FirstChild;
@@ -86,44 +206,111 @@ namespace SpellEbook
                 var css = doc.CreateElement("link");
                 css.SetAttribute("rel", "stylesheet");
                 css.SetAttribute("type", "text/css");
-                css.SetAttribute("href", "../Styles/Style.css");
+                css.SetAttribute("href", "Styles/Style.css");
                 html.FirstChild.AppendChild(css);
                 var body = html.LastChild;
+                body.AppendChild(doc.CreateElement("h1")).AppendChild(doc.CreateTextNode(kv.Key));
 
                 var interestingSpells = kv.Value
                     .GroupBy(s => s.GetSpellLevels().Find(sl => String.Equals(kv.Key, sl.Key, StringComparison.OrdinalIgnoreCase)).Value)
                     .OrderBy(g => g.Key);
                 foreach (var grouping in interestingSpells)
                 {
-                    var header = doc.CreateElement("h1");
+                    var header = doc.CreateElement("h2");
                     header.AppendChild(doc.CreateTextNode($"Level {grouping.Key} spells"));
                     body.AppendChild(header);
-                    var table = doc.CreateElement("table");
-                    body.AppendChild(table);
+                    //var table = doc.CreateElement("table");
+                    //body.AppendChild(table);
                     foreach (var entry in grouping.OrderBy(s => s.Name))
                     {
-                        var row = doc.CreateElement("tr");
-                        table.AppendChild(row);
-                        var col = doc.CreateElement("td");
-                        row.AppendChild(col);
+                        //var row = doc.CreateElement("tr");
+                        //table.AppendChild(row);
+                        //var col = doc.CreateElement("td");
+                        //row.AppendChild(col);
+
                         var link = doc.CreateElement("a");
                         link.AppendChild(doc.CreateTextNode(entry.Name));
-                        link.SetAttribute("href", $"{entry.Name}.xhtml");
-                        col.AppendChild(link);
+                        link.SetAttribute("href", CreateEntryName($"{entry.Name}.xhtml"));
+
+                        var par = doc.CreateElement("p");
+                        body.AppendChild(par);
+                        par.AppendChild(link);
+                        if (!string.IsNullOrWhiteSpace(entry.Summary))
+                        {
+                            par.AppendChild(doc.CreateElement("br"));
+                            par.AppendChild(doc.CreateTextNode(entry.Summary));
+                        }
+                        /*col.AppendChild(link);
                         col = doc.CreateElement("td");
                         row.AppendChild(col);
-                        col.AppendChild(doc.CreateTextNode(entry.Summary));
+                        col.AppendChild(doc.CreateTextNode(entry.Summary));*/
                     }
                 }
 
-                using (var stream = new StreamWriter(File.Open(Path.Combine(dir.FullName, "OEBPS", $"{kv.Key}.xhtml"), FileMode.Create)))
+                var zipEntry = archive.CreateEntry(CreateEntryName($"OEBPS/{kv.Key}.xhtml"));
+                using (var writer = new StreamWriter(zipEntry.Open(), Encoding.UTF8))
                 {
-                    stream.Write(XhtmlHeader);
-                    doc.ToHtml(stream, new AngleSharp.XHtml.XhtmlMarkupFormatter());
+                    writer.Write(XhtmlHeader);
+                    doc.ToHtml(writer, new AngleSharp.XHtml.XhtmlMarkupFormatter());
+                }
+
+                var item = manifestFragment.OwnerDocument.CreateElement("item", opfNS);
+                manifestFragment.AppendChild(item);
+                item.SetAttribute("id", kv.Key);
+                item.SetAttribute("href", zipEntry.Name);
+                item.SetAttribute("media-type", "application/xhtml+xml");
+                var itemRef = spineFragment.OwnerDocument.CreateElement("itemref", opfNS);
+                spineFragment.AppendChild(itemRef);
+                itemRef.SetAttribute("idref", kv.Key);
+
+                var navPoint = navMap.OwnerDocument.CreateElement("navPoint", ncxNS);
+                navMap.AppendChild(navPoint);
+                navPoint.SetAttribute("id", kv.Key);
+                navPoint.SetAttribute("playOrder", $"{navMap.ChildNodes.Count}");
+                var navLabel = navMap.OwnerDocument.CreateElement("navLabel", ncxNS);
+                navPoint.AppendChild(navLabel);
+                navLabel.AppendChild(navMap.OwnerDocument.CreateElement("text", ncxNS)).AppendChild(navMap.OwnerDocument.CreateTextNode(kv.Key));
+                var content = navMap.OwnerDocument.CreateElement("content", ncxNS);
+                navPoint.AppendChild(content);
+                content.SetAttribute("src", CreateEntryName($"{kv.Key}.xhtml"));
+            }
+
+            manifest.AppendChild(manifestFragment);
+            spine.InsertBefore(spineFragment, spine.FirstChild);
+
+
+            var settings = new XmlWriterSettings
+            {
+                Encoding = Encoding.UTF8,
+                Indent = true,
+                IndentChars = "  ",
+                CloseOutput = true
+            };
+            var manifestEntry = archive.CreateEntry("OEBPS/content.opf");
+            using (var writer = XmlWriter.Create(manifestEntry.Open(), settings))
+            {
+                manifest.OwnerDocument.WriteTo(writer);
+            }
+
+            var ncxEntry = archive.CreateEntry("OEBPS/toc.ncx");
+            using (var writer = XmlWriter.Create(ncxEntry.Open(), settings))
+            {
+                navMap.OwnerDocument.WriteTo(writer);
+            }
+        }
+
+        private static string CreateEntryName(string name)
+        {
+            var fileNameChars = name.ToCharArray();
+            for (var i = 0; i < fileNameChars.Length; i++)
+            {
+                if (BadChars.Contains(fileNameChars[i]))
+                {
+                    fileNameChars[i] = '_';
                 }
             }
 
-            
+            return new string(fileNameChars);
         }
 
         private static void GenerateBoilerplate(ZipArchive archive, List<Spell> spells)
@@ -134,7 +321,7 @@ namespace SpellEbook
         }
 
         private static void GenerateCss(ZipArchive archive) {
-            var entry = archive.CreateEntry("Styles/Style.css");
+            var entry = archive.CreateEntry("OEBPS/Styles/Style.css");
             using (var output = entry.Open())
             using (var input = File.OpenRead("content/Styles/Style.css"))
             {
@@ -146,7 +333,7 @@ namespace SpellEbook
         {
             const string content = "application/epub+zip";
             var entry = archive.CreateEntry("mimetype", CompressionLevel.NoCompression);
-            using (var mimeType = new StreamWriter(entry.Open(), Encoding.UTF8))
+            using (var mimeType = new StreamWriter(entry.Open(), Encoding.ASCII))
             {
                 mimeType.Write(content);
             }
@@ -157,11 +344,12 @@ namespace SpellEbook
             var ns = "urn:oasis:names:tc:opendocument:xmlns:container";
             var root = container.CreateElement("container", ns);
             container.AppendChild(root);
+            root.SetAttribute("version", "1.0");
             var rootFiles = container.CreateElement("rootfiles", ns);
             root.AppendChild(rootFiles);
             var rootFile = container.CreateElement("rootfile", ns);
             rootFiles.AppendChild(rootFile);
-            rootFile.SetAttribute("full-path", "content.opf");
+            rootFile.SetAttribute("full-path", "OEBPS/content.opf");
             rootFile.SetAttribute("media-type", "application/oebps-package+xml");
 
             var entry = archive.CreateEntry("META-INF/container.xml");
@@ -186,7 +374,7 @@ namespace SpellEbook
             var style = doc.CreateElement("link");
             style.SetAttribute("rel", "stylesheet");
             style.SetAttribute("type", "text/css");
-            style.SetAttribute("href", "../Styles/Style.css");
+            style.SetAttribute("href", "Styles/Style.css");
             head.AppendChild(title);
             head.AppendChild(style);
             html.AppendChild(head);
